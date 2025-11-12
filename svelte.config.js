d93630c948ca7a90350f609358f1e968576b1403
Bog <bogsacha@gmail.com>
2025-11-12 08:06:22 +0100

    Update Svelte configuration for static adapter and prerendering.

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,24 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/financial-contribution-calculator' : ''
+		},
+
+		prerender: {
+			handleMissingId: 'ignore'
+		}
+	}
 };
 
 export default config;
